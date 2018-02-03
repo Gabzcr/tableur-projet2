85473a731aa44516605966c6003c81543e09daab
Victor Boone <victor.boone@ens-lyon.fr>
2018-02-03 19:37:28 +0100

    Nouveaux tests - 'auto_test.sh' remodelé

--- auto_tests.sh
+++ auto_tests.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
 
+tests_dir="tests/"
+sol_dir="solutions/"
 make
 
+found=true
 echo "\n **************** Starting Tests *****************"
-for test in $(ls tests/)
+for test in $(ls $tests_dir)
 do
-	echo "Testing $test :" `./main.native < tests/$test`
+	res=$(./main.native < $tests_dir/$test)
+	printf "Testing %-10s : %10s" $test $res
+	if [ -e $sol_dir/$test ]
+	then
+		solution=$(cat $sol_dir/$test)
+		if [ $solution = $res ]
+		then
+			echo "\t\t--Right Answer"
+		else
+			echo -n "\t\t--Wrong Answer"
+			printf " - should be \"%s\"\n" $solution 
+		fi
+	else
+		echo "\t\tERROR : $sol_dir/$test"
+	fi
 done;
